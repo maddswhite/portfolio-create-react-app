6e6bc969854989118f3734db27a4b5eaab0ad88a
Madison White <maddywhite@live.ca>
2022-05-31 12:53:11 -0500

    First commit

---
+++ src/components/UserHeader/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  @media (max-width: 640px) {
+    display: ${({ isHome }) => (!isHome ? 'none' : 'flex')};
+    flex-direction: column;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+
+  @media (max-width: 640px) {
+    flex-direction: column;
+  }
+`;
+
+export const Image = styled.img`
+  width: 200px;
+  margin-right: 1rem;
+  border-radius: 2px;
+`;
+
+export const ViewResumeLink = styled.a`
+  display: flex;
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font-weight: bold;
+  align-items: center;
+  margin-top: 1rem;
+  border: 2px solid #00ced1;
+  background-color: #66cdaa;
+  transition: background-color 250ms ease;
+
+  &:hover {
+    background-color: #48d1cc;
+  }
+
+  svg {
+    fill: white;
+    margin-left: 10px;
+  }
+`;
